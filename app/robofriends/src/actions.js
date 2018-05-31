eece51fd44685a6a10da7f73eb05f5264b899f5b
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2018-05-31 13:02:28 +0300

    small changes

--- app/robofriends/src/actions.js
+++ app/robofriends/src/actions.js
@@ -8,7 +8,6 @@ export const setSearchField = (text) =>
 
 export const requestRobots = () => (dispatch) => {
 
-
   dispatch({type: REQUEST_ROBOTS_PENDING});
 
   fetch('https://jsonplaceholder.typicode.com/users')
--- app/robofriends/src/index.js
+++ app/robofriends/src/index.js
@@ -7,11 +7,11 @@ import 'tachyons';
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { searchRobots, requestRobots } from './reducers.js';
-import {createLogger} from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 const logger = createLogger();
 
-const rootReducer = combineReducers({searchRobots, requestRobots});
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,
   logger));
